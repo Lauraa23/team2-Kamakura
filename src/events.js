b3ab8b489fddec5fd77a3f7097b9a76ca5cc3b3c
CarolBV <carolina.botella.valero@gmail.com>
2024-06-05 09:37:28 +0200

    filter

--- src/events.js
+++ src/events.js
@@ -1,2 +1,25 @@
 //Intenta separar los eventos en este archivo.
 
+import { createfilterButtons } from "./menu.js";
+createfilterButtons();
+
+//Intenta separar los eventos en este archivo.
+
+import {searchProducts} from "./searcher.js";
+
+const buttons = document.getElementsByClassName('filter');
+
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener('click', function() {
+  
+        let filtro = buttons[i].value;
+
+        searchProducts({filtro});
+    });
+    
+}
+
+
+
+
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,19 @@
 //DEBE imprimir en pantalla la información de filtros.
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+import { filters, products } from "../assets/data/data.js";
+
+export function createfilterButtons() {
+    const filtersContainer = document.getElementById("filters");
+ filtersContainer.innerHTML = "";
+
+filters.forEach((filter) => {
+const button = document.createElement("button");
+button.className = "filter";
+button.textContent = filter;
+button.value = filter
+filtersContainer.appendChild(button);
+
+});
+}
+
