ceeb31bdc54153bde0f12bea4ee752899dbef03b
Laura Pino <lpigueroa@gmail.com>
2024-06-06 12:38:04 +0200

    cambio nulo

--- src/events.js
+++ src/events.js
@@ -1,33 +1,25 @@
 //Intenta separar los eventos en este archivo.
 
 import { createFilterButtons, createProductCards } from "./menu.js";
-import {searchProducts} from "./searcher.js";
+import { searchProducts } from "./searcher.js";
 import { products } from "../assets/data/data.js";
+
 
 document.addEventListener("DOMContentLoaded", () => {
   createFilterButtons();
   createProductCards(products);
 
+  //Intenta separar los eventos en este archivo.
 
-//Intenta separar los eventos en este archivo.
+  const buttons = document.getElementsByClassName("filter");
 
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+      let filter = buttons[i].textContent;
 
-const buttons = document.getElementsByClassName('filter');
-
-for (let i = 0; i < buttons.length; i++) {
-
-    buttons[i].addEventListener('click', function() {
-  
-        let filter = buttons[i].textContent;
-
-        searchProducts({filter});
+      searchProducts({ filter });
     });
-    
-}
-
-
+  }
 });
-
-
 
 
